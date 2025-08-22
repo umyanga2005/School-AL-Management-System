4e2d87fcedb3b8c69ddace932a7e39f36184e42e
Umayanga Karunarathna <isurukarunarathna2050@gmail.com>
2025-08-22 16:40:49 +0530

    server file updated for local use

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://sccalattendace.netlify.app'], // Vite and Create React App ports
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
