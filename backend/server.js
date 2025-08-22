ac565fb5523ef99aaddddfe011b501d533be3bec
Umayanga Karunarathna <isurukarunarathna2050@gmail.com>
2025-08-22 17:35:04 +0530

    files updated for web online services support

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.DEV_URL,
   credentials: true
 }));
 app.use(express.json());
